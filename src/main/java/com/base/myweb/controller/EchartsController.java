0d8242d21da37f73f7ca14c039ba856564bf59d7
Administrator <272754994@qq.com>
2019-03-11 22:08:37 +0800

    echarts集成

--- src/main/java/com/base/myweb/controller/EchartsController.java
+++ src/main/java/com/base/myweb/controller/EchartsController.java
@@ -1,7 +1,10 @@
 package com.base.myweb.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EchartsController {
@@ -13,5 +16,36 @@ public class EchartsController {
         return "EchartsPage";
     }
 
+    @ResponseBody
+    @RequestMapping("getEchartsInfo")
+    public JSONObject getEchartsInfo(){
+        JSONObject jo = new JSONObject();
+        JSONArray xAxisDataJa = new JSONArray();
+        xAxisDataJa.add("衬衫");
+        xAxisDataJa.add("羊毛衫");
+        xAxisDataJa.add("雪纺衫");
+        xAxisDataJa.add("裤子");
+        xAxisDataJa.add("高跟鞋");
+        xAxisDataJa.add("袜子");
+        jo.put("xAxisData",xAxisDataJa);
+        JSONArray varJa = new JSONArray();
+        varJa.add("150");
+        varJa.add("200");
+        varJa.add("36");
+        varJa.add("100");
+        varJa.add("108");
+        varJa.add("209");
+        jo.put("xVal",varJa);
+
+        JSONArray varJa1 = new JSONArray();
+        varJa1.add("18");
+        varJa1.add("15");
+        varJa1.add("100");
+        varJa1.add("50");
+        varJa1.add("70");
+        varJa1.add("60");
+        jo.put("xVal1",varJa1);
+        return jo;
+    }
 
 }
