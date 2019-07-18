61311db0cdd022f72f9e2b72519c6397da21b2c3
Administrator <272754994@qq.com>
2019-07-18 23:38:30 +0800

    增加安全授权模块，后续完善登陆功能

---
+++ src/main/java/com/base/myweb/core/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.base.myweb.core.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index","/images/**","/js/**","/layui/**","/mods/**","/plugins/**").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/question/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error")
+                .and()
+                .exceptionHandling().accessDeniedPage("/401");
+        http.logout().logoutSuccessUrl("/");
+    }
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+}
